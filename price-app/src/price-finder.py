8efd3ebaa57517480865f8ea88ba6a8f7fbcb39a
Suchitra <suchitra.raghupathi@gmail.com>
2020-06-08 22:45:21 +0530

    Initial commit - learn basic bs4

---
+++ price-app/src/price-finder.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+'''
+https://www.bigbasket.com/pd/10000200/fresho-tomato-hybrid-1-kg/
+<td data-qa="productPrice" class="IyLvo">Rs <!-- -->18</td>
+'''
+url = 'https://www.bigbasket.com/pd/10000200/fresho-tomato-hybrid-1-kg/'
+request = requests.get(url)
+soup = BeautifulSoup(request.content, 'html.parser')
+
+price = soup.find('td', {'data-qa': 'productPrice'})
+print(price.text.strip())
+
+item = soup.find('h1', {'class': 'GrE04'})
+print(item.text.strip())
