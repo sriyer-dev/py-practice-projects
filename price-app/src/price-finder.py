b2d69d3a5739440ca887ef754c60b298a4854dff
sriyer-dev <suchitra.raghupathi@gmail.com>
2020-06-08 23:00:41 +0530

    added comments

--- price-app/src/price-finder.py
+++ price-app/src/price-finder.py
@@ -2,9 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 '''
+Simple program to demonstrate the use of beautiful soup to pull the price of a product from a web page:
 https://www.bigbasket.com/pd/10000200/fresho-tomato-hybrid-1-kg/
 <td data-qa="productPrice" class="IyLvo">Rs <!-- -->18</td>
 '''
+
 url = 'https://www.bigbasket.com/pd/10000200/fresho-tomato-hybrid-1-kg/'
 request = requests.get(url)
 soup = BeautifulSoup(request.content, 'html.parser')
